cff13d6b90c337bbe84e72a00958ba1feca825fe
corey clippinger <coreyclip@gmail.com>
2018-05-05 16:52:57 -0700

    switched to not use reflection

--- data_engineering.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 30 21:18:32 2018
-
-@author: corey
-"""
-
-import pandas as pd
--- flask_app.py
+++ flask_app.py
@@ -1,19 +1,28 @@
 
 # dependencies
-
 from flask import Flask, render_template, jsonify
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 
+app = Flask(__name__)
 
 db_url = "sqlite:///belly_button_biodiversity.sqlite"
 
-app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 db = SQLAlchemy(app)
+
+class otu(db.Model):
+    __tablename__ = 
+
+# Create database tables
+@app.before_first_request
+def setup():
+    # Recreate database each time for demo
+    # db.drop_all()
+    db.create_all()
 
 @app.route("/")
 def home_page():
@@ -21,7 +30,6 @@ def home_page():
 
 @app.route("/sampleNames")
 def get_sample_names():
-
 
     # create engine that connects us to the database
     engine = create_engine(db_url, echo='debug')
@@ -42,8 +50,57 @@ def get_sample_names():
 
     return jsonify(sample_names)
 
+
 @app.route("/otu")
 def get_otu():
+    # create engine that connects us to the database
+    engine = create_engine(db_url, echo='debug')
+
+    # use automap_base so that we don't have to define classes for our tables
+    Base = automap_base()
+
+    Base.prepare(engine, reflect=True)
+
+    #extract the otu which has the sample names
+    otu = Base.classes.otu
+
+    #create a session
+    session = Session(engine)
+
+    #retrieve the sample names
+    unit_names = [str(x) for x in session.query(otu.taxonomic_unit).all()]
+
+    return jsonify(unit_names)
+
+
+@app.route("/metadata/<sample>")
+def get_sample_meta(sample):
+        # create engine that connects us to the database
+    engine = create_engine(db_url, echo='debug')
+
+    # use automap_base so that we don't have to define classes for our tables
+    Base = automap_base()
+
+    Base.prepare(engine, reflect=True)
+
+    #extract the samples_metadata which has the sample names
+    samples_metadata = Base.classes.samples_metadata
+
+    #create a session
+    session = Session(engine)
+
+    final_json = session.query(samples_metadata.AGE,
+                               samples_metadata.BBTYPE,
+                               samples_metadata.ETHNICITY,
+                               samples_metadata.GENDER,
+                               samples_metadata.LOCATION,
+                               samples_metadata.SAMPLEID
+                               ).filter(samples_metadata.SAMPLEID == sample)
+    print(final_json)
+    return jsonify(final_json)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
