52be7715c1491b221a9791697350265494e3ef2a
Corey Clippinger <coreyclip@gmail.com>
2018-05-10 19:46:50 -0700

    Update flask_app.py

--- Belly_Button_Dashboard/flask_app.py
+++ Belly_Button_Dashboard/flask_app.py
@@ -215,7 +215,7 @@ def get_samples(sample):
     return jsonify(final_dict)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
 
 
 
